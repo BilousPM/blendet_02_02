fdec5a3d4d3f197195e0ee1895fd9ec4d98cda23
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-07-14 15:51:36 +0200

    Photos start

--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,10 +1,35 @@
-// import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { getPhotos } from 'apiService/photos';
+import { Form, Text } from 'components';
+import { useEffect, useState } from 'react';
 
 export const Photos = () => {
+  const [query, setQuery] = useState('');
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [photos, setPhotos] = useState([]);
+
+  const onSubmit = value => {
+    setQuery(value);
+  };
+
+  useEffect(() => { 
+    if(!query) return
+    const getData = async () => {
+      try {
+        const data = await getPhotos(query, page);
+        setPhotos(data.photos);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+    getData();
+  }, [query, page]);
+
   return (
     <>
+      <Form onSubmit={onSubmit} />
       <Text textAlign="center">Let`s begin search 🔎</Text>
+     {error && <Text textAlign="center">{error}</Text>}
     </>
   );
 };
