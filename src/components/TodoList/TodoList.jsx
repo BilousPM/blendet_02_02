06e75301e32f6141b2d3a8efa15539ab70bb4db2
Ruslan Garmash <garmash.rb@gmal.com>
2024-07-14 16:03:16 +0300

    2 task complete

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,10 +1,16 @@
 import { Grid, TodoListItem } from '..';
 
-export const TodoList = ({ todos }) => {
+export const TodoList = ({ todos, deleteTodos }) => {
   return (
     <Grid>
       {todos.map(({ text, id }, index) => (
-        <TodoListItem key={id} text={text} index={index} />
+        <TodoListItem
+          key={id}
+          text={text}
+          index={index}
+          deleteTodos={deleteTodos}
+          id={id}
+        />
       ))}
     </Grid>
   );
